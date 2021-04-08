db2fb3efd6849c35c84c3ecf96584bff7b03b27c
results <sejteit@gmail.com>
2021-04-08 16:59:46 -0400

    NIO challenge

--- src/pack/nio/NIOChannelBuffer.java
+++ src/pack/nio/NIOChannelBuffer.java
@@ -1,6 +1,8 @@
 package pack.nio;
 
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.nio.channels.FileChannel;
@@ -8,23 +10,33 @@ import java.nio.channels.FileChannel;
 public class NIOChannelBuffer {
 
 	public static void main(String[] args) throws IOException {
-		//String path = Paths.get(".\\nio.txt").;
+		channelIO();
+	}
+	
+	
+	public static void channelIO() throws IOException {
 		FileInputStream fileInputStream = null;
+		FileOutputStream fileOutputStream = null;
 		FileChannel channel = null;
+		FileChannel channelOut = null;
 		ByteBuffer buffer = null;
 		try {
 			fileInputStream = new FileInputStream(".\\test.txt");
+			fileOutputStream = new FileOutputStream(".\\transfer.txt");
 			channel = fileInputStream.getChannel();
-			buffer = ByteBuffer.allocate((int)channel.size());
+			channelOut = fileOutputStream.getChannel();
+			buffer = ByteBuffer.allocate((int)(channel.size()));
 			channel.read(buffer);
 			buffer.flip();
 			while(buffer.hasRemaining()) {
-					System.out.print((char)buffer.get());				
+				channelOut.write(buffer, buffer.position());
 			}
+	        buffer.clear();
 		} finally {
 			fileInputStream.close();
 			channel.close();
-			buffer.clear();
+			fileOutputStream.close();
+			channelOut.close();
 		}
 	}
 
